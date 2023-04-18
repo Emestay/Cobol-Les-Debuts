837db86d826814e2ecfe8d0c2c8730b14c8d6b4e
Emestay <103575914+Emestay@users.noreply.github.com>
2023-04-18 09:55:01 +0200

    OK

---
+++ Kmaro.cob
@@ -0,0 +1,57 @@
+IDENTIFICATION DIVISION.
+PROGRAM-ID. Kmaro.
+
+DATA DIVISION.
+WORKING-STORAGE SECTION.
+01 ENTREE PIC X.
+
+PROCEDURE DIVISION.
+DEBUT.
+       DISPLAY "Appuyez sur Entrée"
+       ACCEPT ENTREE
+       IF ENTREE = CR
+            DISPLAY "Donne-moi ton coeur, baby"
+            DISPLAY "Ton corps, baby yeah"
+            DISPLAY "Donne-moi ton bon vieux funk"
+            DISPLAY "Ton rock, baby"
+            DISPLAY "Ta soul, baby yeah"
+            DISPLAY "Chante avec moi, je veux une femme like you"
+            DISPLAY "Pour m'emmener au bout du monde"
+            DISPLAY "Une femme like you, hey, hey !"
+            DISPLAY "Donne-moi ton cœur, baby"
+            DISPLAY "Ton corps, baby hey"
+            DISPLAY "Donne-moi ton bon vieux funk"
+            DISPLAY "Ton rock, baby"
+            DISPLAY "Ta soul, baby hey"
+            DISPLAY "Chante avec moi, je veux un homme like you"
+            DISPLAY "Bad boy, tu sais qu'tu m'plais"
+            DISPLAY "Un homme like you, hey, hey !"
+            DISPLAY "Quand tu chantes, j'oublie, j'ai plus le moindre souci"
+            DISPLAY "J'ai le mal qui fuit, tu donnes un son à ma vie"
+            DISPLAY "Et puis j'sais pas qu'est-ce qui s'passe"
+            DISPLAY "T'as ce regard dans la face"
+            DISPLAY "Qui me ramene à la case départ, là ou j'suis parti"
+            DISPLAY "Nous ramene à la soiree du bar, quand on est sortis"
+            DISPLAY "Et c'est cette meme complicite qui s'installe"
+            DISPLAY "Oh quand on est sur la scène et qu'on brille sous la "
+           -    "même étoile"
+            DISPLAY "Quand ta voix croise la mienne, que j'ai ta soul dans mes "
+           -    "veines"
+            DISPLAY "Que mon vibe coule dans les tiennes"
+            DISPLAY "Femme, t'es belle et quand tu chantes t'es sexy"
+            DISPLAY "Flash sur elle, rock star, baby"
+            DISPLAY "Ohhh, ohhh... Yeah"
+            DISPLAY "Donne-moi ton cœur, baby"
+            DISPLAY "Ton corps, baby yeah"
+            DISPLAY "Donne-moi ton bon vieux funk"
+            DISPLAY "Ton rock, baby"
+            DISPLAY "Ta soul, baby yeah"
+            DISPLAY "Chante avec moi, je veux une femme like you"
+            DISPLAY "Pour m'emmener au bout du monde"
+            DISPLAY "Une femme like you, hey, hey !"
+            DISPLAY "Donne-moi ton coeur, baby"
+            DISPLAY "Ton corps, baby hey"
+       END-IF
+       STOP RUN.
+
+       
